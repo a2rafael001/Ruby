7cdef0d144b10d54d3e96abe8a0d8a6a0f1f1ce4
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2024-10-12 14:36:06 +0300

    Sobral 4 zadanie vsyo v 1

--- GettingToKnowRuby/number_operations.rb
+++ GettingToKnowRuby/number_operations.rb
@@ -1,24 +1,43 @@
-# Метод для нахождения делителя числа, являющегося взаимно простым с наибольшим количеством цифр
-def divisor_with_max_coprime_digits(number)
-  max_coprime_digits_count = 0
+# number_operations.rb
+
+# Метод 1: Найти количество чисел, взаимно простых с заданным
+def count_coprimes(n)
+  count = 0
+  (1..n).each do |i|
+    count += 1 if n.gcd(i) == 1
+  end
+  count
+end
+
+# Метод 2: Найти сумму цифр числа, делящихся на 3
+def sum_of_digits_divisible_by_3(number)
+  number.to_s.chars.map(&:to_i).select { |digit| digit % 3 == 0 }.sum
+end
+
+# Метод 3: Найти делитель числа, являющийся взаимно простым с наибольшим количеством цифр данного числа
+def coprime_divisor_with_most_digits(number)
+  digits = number.to_s.chars.map(&:to_i)
+  max_coprimes_count = 0
   best_divisor = nil
 
+  # Ищем все делители числа
   (1..number).each do |divisor|
-    next unless number % divisor == 0
-
-    coprime_digits_count = number.to_s.chars.map(&:to_i).count do |digit|
-      gcd(divisor, digit) == 1 && digit != 0
-    end
-
-    if coprime_digits_count > max_coprime_digits_count
-      max_coprime_digits_count = coprime_digits_count
-      best_divisor = divisor
+    if number % divisor == 0
+      # Считаем количество взаимно простых цифр
+      coprimes_count = digits.count { |digit| divisor.gcd(digit) == 1 }
+      
+      # Обновляем наилучший делитель
+      if coprimes_count > max_coprimes_count
+        max_coprimes_count = coprimes_count
+        best_divisor = divisor
+      end
     end
   end
 
   best_divisor
 end
 
-# Пример использования
-number = 210
-puts "Делитель числа #{number}, являющийся взаимно простым с наибольшим количеством цифр: #{divisor_with_max_coprime_digits(number)}"
+# Примеры использования методов:
+puts "Количество чисел, взаимно простых с 10: #{count_coprimes(10)}"
+puts "Сумма цифр числа 12345, делящихся на 3: #{sum_of_digits_divisible_by_3(12345)}"
+puts "Делитель числа 36, являющийся взаимно простым с наибольшим количеством цифр: #{coprime_divisor_with_most_digits(36)}"
