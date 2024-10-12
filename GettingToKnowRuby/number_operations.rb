4f13b13c6f571d5697e1496bec68a72ec8f456eb
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2024-10-12 14:33:05 +0300

    LB1.T4.M2

--- GettingToKnowRuby/number_operations.rb
+++ GettingToKnowRuby/number_operations.rb
@@ -1,17 +1,12 @@
-# Метод для нахождения количества чисел, взаимно простых с заданным
-def count_coprime_numbers(number)
-  count = 0
-  (1...number).each do |i|
-    count += 1 if gcd(number, i) == 1
+# Метод для нахождения суммы цифр числа, делящихся на 3
+def sum_of_digits_divisible_by_3(number)
+  sum = 0
+  number.to_s.chars.map(&:to_i).each do |digit|
+    sum += digit if digit % 3 == 0
   end
-  count
-end
-
-# Метод для нахождения НОД (алгоритм Евклида)
-def gcd(a, b)
-  b == 0 ? a : gcd(b, a % b)
+  sum
 end
 
 # Пример использования
-number = 12
-puts "Количество чисел, взаимно простых с #{number}: #{count_coprime_numbers(number)}"
+number = 12345
+puts "Сумма цифр числа #{number}, делящихся на 3: #{sum_of_digits_divisible_by_3(number)}"
