a4abd64ec8a8d24ca04e9072d073acbd5860509d
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2024-10-19 00:16:21 +0300

    LB1.T5.M1:add method to find the minimum element in an array
    -Implement 'find_min_element' method to find the minimum element in a
    given array
    -The method uses a loop for sequential comparison of array elements
    -Added input functionality for the user to enter an array of numbers
    -Outputs the minimum element found in the array

--- GettingToKnowRuby/number_methods.rb
+++ GettingToKnowRuby/number_methods.rb
@@ -1,86 +1,17 @@
-# number_methods.rb
+#number_methods
 
-# Метод 1: Найти минимальный элемент массива с использованием цикла for
-def find_min_element_for(array)
-  return nil if array.empty?
-  
-  min_element = array[0]
-  
+def find_min_element(array)
+  min = array[0]
   for element in array
-    min_element = element if element < min_element
+    min = element if element < min
   end
-  
-  min_element
+  min
 end
 
-# Метод 1: Найти минимальный элемент массива с использованием цикла while
-def find_min_element_while(array)
-  return nil if array.empty?
-  
-  min_element = array[0]
-  index = 1
-  
-  while index < array.length
-    min_element = array[index] if array[index] < min_element
-    index += 1
-  end
-  
-  min_element
-end
+# Ввод массива с клавиатуры
+puts "Введите числа, разделенные пробелами:"
+input = gets.chomp
+array = input.split.map(&:to_i)
 
-# Метод 2: Найти все положительные элементы массива с использованием цикла for
-def find_positive_elements_for(array)
-  positive_elements = []
-  
-  for element in array
-    positive_elements << element if element > 0
-  end
-  
-  positive_elements
-end
-
-# Метод 2: Найти все положительные элементы массива с использованием цикла while
-def find_positive_elements_while(array)
-  positive_elements = []
-  index = 0
-  
-  while index < array.length
-    positive_elements << array[index] if array[index] > 0
-    index += 1
-  end
-  
-  positive_elements
-end
-
-# Метод 3: Найти индекс первого положительного элемента массива с использованием цикла for
-def find_first_positive_index_for(array)
-  for index in 0...array.length
-    return index if array[index] > 0
-  end
-  
-  nil
-end
-
-# Метод 3: Найти индекс первого положительного элемента массива с использованием цикла while
-def find_first_positive_index_while(array)
-  index = 0
-  
-  while index < array.length
-    return index if array[index] > 0
-    index += 1
-  end
-  
-  nil
-end
-
-# Примеры использования методов:
-array = [-5, -2, 0, 3, 7, -8]
-
-puts "Минимальный элемент массива (for): #{find_min_element_for(array)}"
-puts "Минимальный элемент массива (while): #{find_min_element_while(array)}"
-
-puts "Положительные элементы массива (for): #{find_positive_elements_for(array)}"
-puts "Положительные элементы массива (while): #{find_positive_elements_while(array)}"
-
-puts "Индекс первого положительного элемента массива (for): #{find_first_positive_index_for(array)}"
-puts "Индекс первого положительного элемента массива (while): #{find_first_positive_index_while(array)}"
+# Вывод результата
+puts "Минимальный элемент: #{find_min_element(array)}"
