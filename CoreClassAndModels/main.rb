f536755087bf9808334b8f94c182e0ab4eb656da
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2024-10-19 01:42:36 +0300

    Lb2.T1:create Student class
    -Implement functionality for managing student information
    -Created 'Student'class with
    fields:ID,surname,name,patronymic,phone,telegram,emaeil and git
    -Implemented a constructor to initialize mandatory and optional fields
    -Added getters and setters for each field following Ruby naming
    conventions
    -Implemented a 'to_s' method for formatted output of student information
    -Created a script to instantiate sample students and display their
    information

--- CoreClassAndModels/main.rb
+++ CoreClassAndModels/main.rb
@@ -1,36 +1,12 @@
-# student.rb
+require_relative 'student'
 
-class Student
-  attr_accessor :id, :surname, :first_name, :middle_name, :phone, :telegram, :email, :git
-  
-  # Конструктор класса
-  def initialize(surname, first_name, middle_name, id = nil, phone = nil, telegram = nil, email = nil, git = nil)
-    @id = id
-    @surname = surname
-    @first_name = first_name
-    @middle_name = middle_name
-    @phone = phone
-    @telegram = telegram
-    @email = email
-    @git = git
-  end
+students = [
+  Student.new('Иванов', 'Иван', 'Иванович', 1, '+7 123 456 7890', '@ivanov', 'ivanov@example.com', 'https://github.com/ivanov'),
+  Student.new('Петров', 'Петр', 'Петрович', 2, '+7 987 654 3210', '@petrov', 'petrov@example.com', 'https://github.com/petrov'),
+  Student.new('Сидоров', 'Сидор', 'Сидорович') # Здесь мы не указываем ID и другие необязательные поля
+]
 
-  # Метод для отображения информации о студенте
-  def to_s
-    "ID: #{id || 'N/A'}, ФИО: #{surname} #{first_name} #{middle_name}, Телефон: #{phone || 'N/A'}, " \
-    "Телеграм: #{telegram || 'N/A'}, Почта: #{email || 'N/A'}, Git: #{git || 'N/A'}"
-  end
-end
-# main.rb
-require './student'
-
-# Создаем несколько объектов класса Student
-student1 = Student.new('Иванов', 'Иван', 'Иванович', 1, '+7 123 456 7890', '@ivanov', 'ivanov@example.com', 'https://github.com/ivanov')
-student2 = Student.new('Петров', 'Петр', 'Петрович', 2, '+7 987 654 3210', '@petrov', 'petrov@example.com', 'https://github.com/petrov')
-student3 = Student.new('Сидоров', 'Сидор', 'Сидорович')
-
-# Вывод информации о каждом студенте
 puts "Информация о студентах:"
-puts student1
-puts student2
-puts student3
+students.each do |student|
+  puts student.to_s
+end
--- CoreClassAndModels/student.rb
+++ CoreClassAndModels/student.rb
@@ -1,20 +1,86 @@
-# student.rb
-
 class Student
-  # Конструктор  геттеры/сеттеры  автоматически создаются для  полей
-  attr_accessor :id, :surname, :first_name, :middle_name, :phone, :telegram, :email, :git
-  
-  # Конструктор класса. Фамилия, имя и отчество обязательны, остальные поля не обязательны
-  def initialize(surname, first_name, middle_name, id = nil, phone = nil, telegram = nil, email = nil, git = nil)
+  attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
+
+  # Конструктор класса
+  def initialize(surname, name, patronymic, id = nil, phone = nil, telegram = nil, email = nil, git = nil)
     @id = id
     @surname = surname
-    @first_name = first_name
-    @middle_name = middle_name
+    @name = name
+    @patronymic = patronymic
     @phone = phone
     @telegram = telegram
     @email = email
     @git = git
   end
 
-  # Геттеры и сеттеры для каждого поля автоматически генерируются с помощью attr_accessor
+  # Геттеры
+  def get_id
+    @id
+  end
+
+  def get_surname
+    @surname
+  end
+
+  def get_name
+    @name
+  end
+
+  def get_patronymic
+    @patronymic
+  end
+
+  def get_phone
+    @phone
+  end
+
+  def get_telegram
+    @telegram
+  end
+
+  def get_email
+    @email
+  end
+
+  def get_git
+    @git
+  end
+
+  # Сеттеры
+  def set_id(id)
+    @id = id
+  end
+
+  def set_surname(surname)
+    @surname = surname
+  end
+
+  def set_name(name)
+    @name = name
+  end
+
+  def set_patronymic(patronymic)
+    @patronymic = patronymic
+  end
+
+  def set_phone(phone)
+    @phone = phone
+  end
+
+  def set_telegram(telegram)
+    @telegram = telegram
+  end
+
+  def set_email(email)
+    @email = email
+  end
+
+  def set_git(git)
+    @git = git
+  end
+
+  # Метод для вывода информации о студенте
+  def to_s
+    "ID: #{@id || 'N/A'}, ФИО: #{@surname} #{@name} #{@patronymic}, Телефон: #{@phone || 'N/A'}, Телеграм: #{@telegram || 'N/A'}, Почта: #{@email || 'N/A'}, Git: #{@git || 'N/A'}"
+  end
 end
