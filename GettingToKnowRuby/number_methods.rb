422ddd5d2185b88ed3e62b2587fda187ebc4d4a6
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2024-10-19 00:43:46 +0300

    LB1.T5.M3:add methods for finding min,max,andFirstPositiveElementIndex
    -Implemented 'find_min_element'method to find the minimum element in an
    array using a loop
    -Implemented 'find_max_element'nethod to find the maximum element in an
    array using a while loop
    -Impnemented'index_of_first_positive'method to find the index of the
    first positive element in an array
    -Added input functionality for users to enter an array of numbers
    -Outouts the minimum and maximum elements, as well as the index of the
    first positive element

--- GettingToKnowRuby/number_methods.rb
+++ GettingToKnowRuby/number_methods.rb
@@ -1,5 +1,4 @@
-#number_methods
-#find_min_element
+# Метод для нахождения минимального элемента
 def find_min_element(array)
   min = array[0]
   for element in array
@@ -8,7 +7,7 @@ def find_min_element(array)
   min
 end
 
-
+# Метод для нахождения максимального элемента
 def find_max_element(array)
   max = array[0]
   index = 1
@@ -19,6 +18,16 @@ def find_max_element(array)
   max
 end
 
+# Метод для нахождения индекса первого положительного элемента
+def index_of_first_positive(array)
+  index = 0
+  while index < array.length
+    return index if array[index] > 0
+    index += 1
+  end
+  nil # Возвращаем nil, если положительный элемент не найден
+end
+
 # Ввод массива с клавиатуры
 puts "Введите числа, разделенные пробелами:"
 input = gets.chomp
@@ -27,3 +36,4 @@ array = input.split.map(&:to_i)
 # Вывод результатов
 puts "Минимальный элемент: #{find_min_element(array)}"
 puts "Максимальный элемент: #{find_max_element(array)}"
+puts "Индекс первого положительного элемента: #{index_of_first_positive(array)}"
