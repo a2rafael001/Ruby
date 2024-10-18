697c0c1e63a653970724e02177635aca2434f81b
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2024-10-19 00:32:47 +0300

    LB1.T5.M2:add methods to find minimum and maximum elements in an array
    -Implemented 'find_min_element' method to find the minimum element using
    a loop
    -Implemented 'find_max_element' method to find the maximum element using
    a while loop
    -Added input functional gor users to enter an array of numbers
    -Outouts both the minimum add maximum elements found in the

--- GettingToKnowRuby/number_methods.rb
+++ GettingToKnowRuby/number_methods.rb
@@ -1,5 +1,5 @@
 #number_methods
-
+#find_min_element
 def find_min_element(array)
   min = array[0]
   for element in array
@@ -8,10 +8,22 @@ def find_min_element(array)
   min
 end
 
+
+def find_max_element(array)
+  max = array[0]
+  index = 1
+  while index < array.length
+    max = array[index] if array[index] > max
+    index += 1
+  end
+  max
+end
+
 # Ввод массива с клавиатуры
 puts "Введите числа, разделенные пробелами:"
 input = gets.chomp
 array = input.split.map(&:to_i)
 
-# Вывод результата
+# Вывод результатов
 puts "Минимальный элемент: #{find_min_element(array)}"
+puts "Максимальный элемент: #{find_max_element(array)}"
