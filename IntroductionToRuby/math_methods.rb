cf719807821148262f45e821d378a92ea36453c7
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2024-11-28 16:49:12 +0300

    LB1.T4.M2.Sum of the digits of a number that is divisible by 3
    Added a method for calculating the sum of digits that are multiples of 3
    A function has been implemented to colculate the sum of digits that are
    multiples of 3
    Added logic for summing digits that are multiples of 3
    Implemented the calculation of the sum of digits for numbers that are
    multiples of 3

--- IntroductionToRuby/math_methods.rb
+++ IntroductionToRuby/math_methods.rb
@@ -15,4 +15,15 @@ def count_coprimes(n)
   count
 end
 
-
+# Метод 2: Найти сумму цифр числа, делящихся на 3
+def count_odd_digits_greater_than_3(number)
+  count = 0
+  while number > 0
+    digit = number % 10  # Извлекаем последнюю цифру
+    if digit % 2 != 0 && digit > 3  # Проверяем на нечётность (остаток от деления 2) и больше ли цифра 3
+      count += 1
+    end
+    number /= 10  # Убираем последнюю цифру
+  end
+  count
+end
