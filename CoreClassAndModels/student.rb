13ff40bbd8cfea50623a6df029c1ae19beb54311
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2024-12-19 13:55:46 +0300

    LB2.T1.Add Student class with basic attributes and methods
    -Imlemented the Student class with the following
    attributes:id,last_name,first_name,middle_name,phone,telegram,email and
    git.
    -Made id and full name
    attributes(last_name,first_name,middle_name)mandatory,while others are
    optional.
    -Added a method full_name to retrieve the student's full name in a
    formatted string.
    -Overrode the to_s method to provide a user-friendly string
    representation of the Student object.
    -Included default "not secifid"massages for optional attributes if they
    are nill

--- CoreClassAndModels/student.rb
+++ CoreClassAndModels/student.rb
@@ -1,86 +1,26 @@
 class Student
-  attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
+  attr_accessor :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :github
 
-  # Конструктор класса
-  def initialize(surname, name, patronymic, id = nil, phone = nil, telegram = nil, email = nil, git = nil)
+  # Конструктор: обязательные ФИО, остальные поля опциональны
+  def initialize(id:, last_name:, first_name:, middle_name:, phone: nil, telegram: nil, email: nil, github: nil)
     @id = id
-    @surname = surname
-    @name = name
-    @patronymic = patronymic
+    @last_name = last_name
+    @first_name = first_name
+    @middle_name = middle_name
     @phone = phone
     @telegram = telegram
     @email = email
-    @git = git
+    @github = github
   end
 
-  # Геттеры
-  def get_id
-    @id
+  # Метод для получения полного имени студента
+  def full_name
+    "#{last_name} #{first_name} #{middle_name}"
   end
 
-  def get_surname
-    @surname
-  end
-
-  def get_name
-    @name
-  end
-
-  def get_patronymic
-    @patronymic
-  end
-
-  def get_phone
-    @phone
-  end
-
-  def get_telegram
-    @telegram
-  end
-
-  def get_email
-    @email
-  end
-
-  def get_git
-    @git
-  end
-
-  # Сеттеры
-  def set_id(id)
-    @id = id
-  end
-
-  def set_surname(surname)
-    @surname = surname
-  end
-
-  def set_name(name)
-    @name = name
-  end
-
-  def set_patronymic(patronymic)
-    @patronymic = patronymic
-  end
-
-  def set_phone(phone)
-    @phone = phone
-  end
-
-  def set_telegram(telegram)
-    @telegram = telegram
-  end
-
-  def set_email(email)
-    @email = email
-  end
-
-  def set_git(git)
-    @git = git
-  end
-
-  # Метод для вывода информации о студенте
+  # Переопределение метода to_s для удобного вывода информации
   def to_s
-    "ID: #{@id || 'N/A'}, ФИО: #{@surname} #{@name} #{@patronymic}, Телефон: #{@phone || 'N/A'}, Телеграм: #{@telegram || 'N/A'}, Почта: #{@email || 'N/A'}, Git: #{@git || 'N/A'}"
+    "ID: #{id}\nФИО: #{full_name}\nТелефон: #{phone || 'не указано'}\n" \
+    "Телеграм: #{telegram || 'не указано'}\nПочта: #{email || 'не указано'}\nГит: #{github || 'не указано'}"
   end
 end
