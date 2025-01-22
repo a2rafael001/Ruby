63636a51cf2694b70d6b2e4e475cad51d1078059
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2025-01-22 05:12:15 +0300

    The end GLAVA5

--- ruby1/prices.rb
+++ ruby1/prices.rb
@@ -1,32 +1,28 @@
 def ves(prices)
  summa = 0
- index = 0
- while index < prices.length
- summa += prices[index]
- index += 1
+ prices.each do |prices|
+ summa += prices
  end
  summa
 end
-prices = [3.99, 25.00, 8.99]
-puts format("%.2f", ves(prices))
 
 def vozrat_deneg(prices)
  summa = 0
- index = 0
- while index < prices.length
- summa -= prices[index]
- index += 1
+ prices.each do |prices|
+ summa -= prices
  end
  summa
 end
-puts format("%.2f", vozrat_deneg(prices))
 
 def umenshit_cenu(prices)
- index = 0
- while index < prices.length
- summa_um = prices[index] / 3.0
+ prices.each do |prices|
+ summa_um = prices / 3.0
  puts format("Ваша скидка: $%.2f", summa_um)
- index += 1
  end
 end
+
+prices = [3.99, 25.00, 8.99]
+
+puts format("%.2f", ves(prices))
+puts format("%.2f", vozrat_deneg(prices))
 umenshit_cenu(prices)
