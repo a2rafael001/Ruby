950c3cb89ed44400cf5935c8dcba77b7b721f22a
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2025-01-22 05:19:16 +0300

    lab 3 | task 2.5
    Add 'custom_min_by' and 'custom_find' methods for finding the minimum
    element by block and the first element matching the block condition.

--- blocks_collections_trees/Blocks/processing_array.rb
+++ blocks_collections_trees/Blocks/processing_array.rb
@@ -36,3 +36,26 @@ def custom_inject(initial = nil)
     accumulator
   end
 end
+
+def custom_min_by
+    return nil if @array.empty?
+    min_element = @array.first
+    min_value = yield(min_element)
+
+    @array.each do |element|
+      current_value = yield(element)
+      if current_value < min_value
+        min_element = element
+        min_value = current_value
+      end
+    end
+    min_element
+  end
+
+  def custom_find
+    @array.each do |element|
+      return element if yield(element)
+    end
+    nil
+  end
+end
