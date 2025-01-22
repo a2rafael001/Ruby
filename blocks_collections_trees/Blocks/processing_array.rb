f30661e5f503e21a60bfbd790949897cfa400d25
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2025-01-22 03:55:35 +0300

    lab 3 | task 2.2
    Add `custom_flat_map` method to implement a custom version of the
    `flat_map` functionality with a block.

--- blocks_collections_trees/Blocks/processing_array.rb
+++ blocks_collections_trees/Blocks/processing_array.rb
@@ -7,3 +7,12 @@ class ArrayProcessor
     @array
   end
 end
+
+ def custom_flat_map
+    result = []
+    @array.each do |element|
+      result.concat(yield(element))
+    end
+    result
+  end
+end
