83425aac775694ea02adb935e8110437a7b1910a
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2025-02-18 14:32:28 +0300

    LAB 4 | task 3.3 ERD

---
+++ CoreClassAndModels/data_base/schema_changes/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    last_name VARCHAR(255),
+    first_name VARCHAR(255),
+    middle_name VARCHAR(255),
+    birth_date DATE,
+    phone VARCHAR(15),
+    telegram VARCHAR(255),
+    email VARCHAR(255),
+    git VARCHAR(255)
+);
